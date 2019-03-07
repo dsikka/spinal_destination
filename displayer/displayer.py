396b7918628f8b9c2f31f61ee7c4e9d63b8db250
Dipika Sikka <dipikasikka1@gmail.com>
2019-03-07 15:03:02 -0500

    Update for muliti-marker testing

--- displayer/displayer.py
+++ displayer/displayer.py
@@ -1,12 +1,13 @@
-import os
-import unittest
-import vtk, qt, ctk, slicer
-from qt import *
-from slicer.ScriptedLoadableModule import *
+import csv
 import datetime, time, json
 import logging
 import numpy
-import csv
+import os
+import unittest
+import vtk, qt, ctk, slicer
+
+from qt import *
+from slicer.ScriptedLoadableModule import *
 
 
 #
@@ -43,14 +44,10 @@ class displayerWidget(ScriptedLoadableModuleWidget):
         parametersFormLayout = qt.QFormLayout(parametersCollapsibleButton)
 
         # Define combo box selector member variables:
-        self.transform2OfInterestSelector = slicer.qMRMLNodeComboBox()
-        self.transform2OfInterestSelectorLabel = qt.QLabel()
         self.fiducialOfInterestSelector = slicer.qMRMLNodeComboBox()
         self.fiducialOfInterestSelectorLabel = qt.QLabel()
         self.transformOfInterestSelector = slicer.qMRMLNodeComboBox()
         self.transformOfInterestSelectorLabel = qt.QLabel()
-        self.second_marker_selector = slicer.qMRMLNodeComboBox()
-        self.second_marker_selector_label = qt.QLabel()
 
         # Save To Directory Selector
         self._write_to_dir = ctk.ctkPathLineEdit()
@@ -80,29 +77,6 @@ class displayerWidget(ScriptedLoadableModuleWidget):
                              ["vtkMRMLTransformNode"],
                              "Pick Start Point")
         parametersFormLayout.addRow(self.fiducialOfInterestSelectorLabel, self.fiducialOfInterestSelector)
-
-        # Real World Transform of interest.
-        # This Transform should be a dummy transform not connected to a model in Slicer.
-        # The Transform will receive streaming transform data relating to the aruco
-        # fiducial seen in the camera 3D space.
-        self.create_selector(self.transform2OfInterestSelectorLabel,
-                             self.transform2OfInterestSelector,
-                             "Transform of Interest for Real World: ",
-                             ["vtkMRMLTransformNode"],
-                             "Pick transform of interest for Real World")
-        parametersFormLayout.addRow(self.transform2OfInterestSelectorLabel, self.transform2OfInterestSelector)
-
-        # Real World Transform of interest 2.
-        # This Transform should be a dummy transform not connected to a model in Slicer.
-        # The Transform will receive streaming transform data relating to the aruco
-        # fiducial seen in the camera 3D space. To be used for testing multi marker tracking.
-        self.create_selector(self.second_marker_selector_label,
-                             self.second_marker_selector,
-                             "Transform of Interest 2: ",
-                             ["vtkMRMLTransformNode"],
-                             "Testing Multi Marker Tracking")
-        parametersFormLayout.addRow(self.second_marker_selector_label,
-                                    self.second_marker_selector)
         # start endless button
         self.startEndlessButton = qt.QPushButton("Start")
         self.startEndlessButton.toolTip = "Start"
@@ -174,12 +148,8 @@ class displayerWidget(ScriptedLoadableModuleWidget):
             transformOfInterest = self.transformOfInterestSelector.currentNode()
             # Get currently selected fiducial
             fiducialOfInterest = self.fiducialOfInterestSelector.currentNode()
-            # Get Real World to Marker Transform Node
-            realWorldTransformNode = self.transform2OfInterestSelector.currentNode()
-            realWorldTransformNode2 = self.second_marker_selector.currentNode()
             # Passing in fiducial of interest
-            self.logic.run(transformOfInterest, fiducialOfInterest, realWorldTransformNode, realWorldTransformNode2,
-                           self._write_to_dir.currentPath, self.t_form_nodes)
+            self.logic.run(transformOfInterest, fiducialOfInterest, self._write_to_dir.currentPath, self.t_form_nodes)
         else:
             print('Select Save Directory!')
 
@@ -205,17 +175,11 @@ class displayerLogic(ScriptedLoadableModuleLogic):
         self.cy = 2.3184788479005914e+002
         self.fy = 6.4324331025844515e+002
         self.cx = 3.1163816392087517e+002
-        # self.cy = 2.6188803044119754e+002
-        # self.fy = 5.9780697114621512e+002
-        # self.cx = 3.1953140232090112e+002
-        # self.fx = 5.9596203089288861e+002
         self.fx = 6.4367706296746178e+002
 
         self.transformNodeObserverTags = []
         self.transformOfInterestNode = None
         # Variable for storing the real world transforms as they are streamed
-        self.realWorldTransformNode = None
-        self.realWorldTransformNode2 = None
         # Create Variables for storing the transforms from aruco marker relative to start point, the node for the
         # fiducial node
         self.ctTransform = None
@@ -242,19 +206,49 @@ class displayerLogic(ScriptedLoadableModuleLogic):
         self._start_point_collection = {'time': [], '3D pos': [], '2D pos': []}
         self._transform_dictionary = {''}
 
+        self.realWorldTransformNode_m0 = None
+        self.realWorldTransformNode_m1 = None
+        self.realWorldTransformNode_m2 = None
+        self.realWorldTransformNode_m3 = None
+        self.realWorldTransformNode_m4 = None
+
+        # Should be updated on entry once the aruco cube has been loaded
+        self.offsets = None
+        self.cube_length = 0
+
+        self.transforms = {'Marker0ToTracker': {'node': self.realWorldTransformNode_m0, 'coords': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)},
+        'Marker1ToTracker': {'node': self.realWorldTransformNode_m1, 'coords': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)},
+        'Marker2ToTracker': {'node': self.realWorldTransformNode_m2, 'coords': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)},
+        'Marker3ToTracker': {'node': self.realWorldTransformNode_m3, 'coords': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)},
+        'Marker4ToTracker': {'node': self.realWorldTransformNode_m4, 'coords': (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)}}
+
+    def euc_distance(parent_matrix, matrix):
+        x, y, z = parent_matrix.GetElement(0, 3), parent_matrix.GetElement(1, 3), parent_matrix.GetElement(2, 3)
+        x2, y2, z2 = matrix.GetElement(0, 3), matrix.GetElement(1, 3), matrix.GetElement(2, 3)
+        #print(x, y, z, x2, y2, z2)
+        return np.sqrt((x - x2) ** 2 + (y - y2) ** 2 + (z - z2) ** 2)
+
+    def updateOffsets(self):
+        # Dimensions are in mm
+        bounds = [0,0,0,0,0,0]
+        cube = slicer.util.getNode('ArucoCube')
+        cube.GetRASBounds(bounds)
+        self.cube_length = np.absolute(bounds[1] - bounds[0])
+
+        self.offsets = {'Marker0ToTracker': {'matrix': None, 'transform': None, 'offset': (1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, self.cube_length/2, 0, 0, 0, 1)},
+        'Marker1ToTracker': {'matrix': None, 'transform': None, 'offset': (-1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 1, self.cube_length/2, 0, 0, 0, 1)},
+        'Marker2ToTracker': {'matrix': None, 'transform': None, 'offset': (0, 0, 1, 0, 0, -1, 0, 0, 1, 0, 0, self.cube_length/2, 0, 0, 0, 1)},
+        'Marker3ToTracker': {'matrix': None, 'transform': None, 'offset': (1, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, self.cube_length/2, 0, 0, 0, 1)},
+        'Marker4ToTracker': {'matrix': None, 'transform': None, 'offset': (0, 0, -1, 0, 0, -1, 0, 0, -1, 0, 0, self.cube_length/2, 0, 0, 0, 1)}}
+
     def addObservers(self):
         transformModifiedEvent = 15000
-        nodes = [self.realWorldTransformNode, self.realWorldTransformNode2]
-        funcs = [self.onTransformOfInterestNodeModified, self.on_transform_2_modified]
-        for _, (node, func) in enumerate(zip(nodes, funcs)):
-            transformNode = node
-            while transformNode:
-                print
-                "Add observer to {0}".format(transformNode.GetName())
-                self.transformNodeObserverTags.append(
-                    [transformNode,
-                     transformNode.AddObserver(transformModifiedEvent, func)])
-                transformNode = transformNode.GetParentTransformNode()
+        for (k,v) in self.transforms.items():
+            transform_node = v['node']
+            while transform_node:
+                print "Add observer to {0}".format(transform_node.GetName())
+                self.transformNodeObserverTags.append([transform_node, transform_node.AddObserver(transformModifiedEvent, self.onTransformOfInterestNodeModified)])
+                transform_node = transform_node.GetParentTransformNode()
 
     def removeObservers(self):
         print("Remove observers")
@@ -262,38 +256,72 @@ class displayerLogic(ScriptedLoadableModuleLogic):
             nodeTagPair[0].RemoveObserver(nodeTagPair[1])
 
     def onTransformOfInterestNodeModified(self, observer, eventId):
-        if self.spInMarker is not None:
-            print('Made it here')
+        # Calculate the expected distance between two cude faces
+        validNodes = []
+        expected = self.cube_length * np.sqrt(2) / 2
+        name = observer.GetName()
+        parent_matrix, parent_transform = self.create_4x4_vtk_mat_from_node(self.transforms[name]['node'])
+        self.offsets[name]['matrix'] = parent_matrix
+        self.offsets[name]['transform'] = parent_transform
+        validNodes = [name]
+        for (k,v) in self.transforms.items():
+            matrix, transform_real_world_interest = self.create_4x4_vtk_mat_from_node(v['node'])
+            
+            # Better check?
+            x, y, z = matrix.GetElement(0, 3), matrix.GetElement(1, 3), matrix.GetElement(2, 3)
+            data_sum = x + y + z
             # Create matrix to store the transform for camera to aruco marker
-            matrix, transform_real_world_interest = self.create_4x4_vtk_mat_from_node(self.realWorldTransformNode)
-            # Multiply start point in marker space calculated form the CT model by the
-            # camera to aruco transform to get the start point in 3D camera space.
-            startPointinCamera = matrix.MultiplyPoint(self.spInMarker)
-
+            if data_sum > 0 and k != name:
+                print 'parent: ', name
+                print 'other: ', k
+                # Compare matrix of current node to that of the observer
+                # if within some bounds, add it to the list of validNodes
+                # if not within bounds, do not add
+                face_distance = euc_distance(parent_matrix, matrix)
+                print 'distance: ', face_distance
+                if np.abs(face_distance - expected) < 50:
+                    validNodes.append(k)
+                    v['transform'] = transform_real_world_interest
+                    v['matrix'] = matrix
+            
+        print validNodes
+        Xc_sum = 0
+        Yc_sum = 0
+        Zc_sum = 0
+        Xc2_sum = 0
+        Yc2_sum = 0
+        Zc2_sum = 0
+        for j in range(len(validNodes)):
+            # Get the appropriate offset (based on the face of the cube)
+            # Apply it to the stored start point, which is currently at the center of the cube
+            offset = self.offsets[validNodes[j]]['offset']
+            offset_matrix = vtk.vtkMatrix4x4()
+            offset_matrix.DeepCopy(offset)
+            curr_marker = offset_matrix.MultiplyPoint(self.spInMarker)
+            
+            matrix = self.offsets[validNodes[j]]['matrix']
+            startPointinCamera = matrix.MultiplyPoint(curr_marker)
+            
             # Set calculated 3d start point in camera space
-            Xc = startPointinCamera[0]
-            Yc = startPointinCamera[1]
-            Zc = startPointinCamera[2]
-
-            # Perform 3D (Camera) to 2D project sp
-            x, y = self.transform_3d_to_2d(Xc, Yc, Zc)
-
+            
+            Xc_sum += startPointinCamera[0]
+            Yc_sum += startPointinCamera[1]
+            Zc_sum += startPointinCamera[2]
             # Get Marker 3D
-            Xc2, Yc2, Zc2 = get_3d_coordinates(transform_real_world_interest)
-
-            # Perform 3D (Camera) to 2D project
-            x2, y2 = self.transform_3d_to_2d(Xc2, Yc2, Zc2)
-
-            # Save to Collection
+            Xc2, Yc2, Zc2 = self.get_3d_coordinates(self.offsets[validNodes[j]]['transform'])
+            
             t = time.time()
             self._marker_1_collection['time'].append(t)
             self._marker_1_collection['3D pos'].append([Xc2, Yc2, Zc2])
-            self._marker_1_collection['2D pos'].append([x2, y2])
-
-            self._start_point_collection['time'].append(t)
-            self._start_point_collection['3D pos'].append([Xc, Yc, Zc])
-            self._start_point_collection['2D pos'].append([x, y])
-
+            self._start_point_collection['3D pos'].append([startPointinCamera[0], startPointinCamera[1], startPointinCamera[2]])
+            
+            Xc2_sum += Xc2
+            Yc2_sum += Yc2
+            Zc2_sum += Zc2
+            # Perform 3D (Camera) to 2D project
+            # Use the average to determine the startpoint
+            # Save to Collection
+            
             # todo:Apply distortion values
             print('camera point', x, y)
             print('marker', x2, y2)
@@ -305,55 +333,53 @@ class displayerLogic(ScriptedLoadableModuleLogic):
             # Setup Marker Matrix
             vtk_marker_matrix = self.create_4x4_vtk_mat(x2, y2)
 
-            # Update Nodes
-            #self.startPointSphere.SetMatrixTransformToParent(vtk_sp_matrix)
-            #self.displayMarkerSphere.SetMatrixTransformToParent(vtk_marker_matrix)
-
-            # Extract Rotation:
-            # rotation_matrix = vtk.vtkMatrix4x4()
-            # rotation_matrix.DeepCopy(matrix)
-            # for i in range(0, 3):
-            #     r_1 = rotation_matrix.GetElement(0, i)
-            #     r_2 = rotation_matrix.GetElement(1, i)
-            #     r_3 = rotation_matrix.GetElement(2, i)
-            #     mag = numpy.sqrt(
-            #         r_1 ** 2 + r_2 ** 2 + r_3 ** 2)
-            #     rotation_matrix.SetElement(0, i, r_1 / mag)
-            #     rotation_matrix.SetElement(1, i, r_2 / mag)
-            #     rotation_matrix.SetElement(2, i, r_3 / mag)
-            #
-            # rotation_matrix.SetElement(0, 3, x)
-            # rotation_matrix.SetElement(1, 3, y)
-            # rotation_matrix.SetElement(2, 3, 1)
-            # rotation_matrix.SetElement(2, 3, 1)
-
             for cyl in self.display_marker_cylinders:
                 cyl.SetMatrixTransformToParent(vtk_sp_matrix)
-            #print(vtk_sp_matrix.__str__())
+
+        Xc2 = Xc2_sum/len(validNodes)
+        Yc2 = Yc2_sum/len(validNodes)
+        Zc2 = Zc2_sum/len(validNodes)
+    
+        Xc = Xc_sum/len(validNodes)
+        Yc = Yc_sum/len(validNodes)
+        Zc = Zc_sum/len(validNodes)
+    
+        [x2, y2] = [0, 0]
+        [x2, y2] = self.transform_3d_to_2d(Xc2, Yc2, Zc2)
+        self._marker_1_collection['2D pos'].append([x2, y2])
+    
+        # Perform 3D (Camera) to 2D project sp
+        [x, y] = [0, 0]
+        [x, y] = self.transform_3d_to_2d(Xc, Yc, Zc)
+        self._start_point_collection['2D pos'].append([x, y])
+
+    
+        vtk_sp_matrix = self.create_4x4_vtk_mat(x, y)
+        # Setup Marker Matrix
+        vtk_marker_matrix = self.create_4x4_vtk_mat(x2, y2)
+        # Update Nodes
+    
+        # ONLY CURRENTLY SHOWING ONE DISPLAY NODE
+        # WILL UPDATE WHEN DISPLAY IS FINALIZED
+        #self.startPointSphere.SetMatrixTransformToParent(vtk_sp_matrix)
+        #self.displayMarkerSphere.SetMatrixTransformToParent(vtk_marker_matrix)
+        print "Start point number: ", i
+        print x, y
+
+    def addTransforms(self):
+        for (k,v) in self.transforms.items():
+            matrix = vtk.vtkMatrix4x4()
+            matrix.DeepCopy(v['coords'])
+            transform = slicer.vtkMRMLLinearTransformNode()
+            transform.SetName(k)
+            slicer.mrmlScene.AddNode(transform)
+            transform.ApplyTransformMatrix(matrix)
+            v['node'] = transform
 
     def transform_3d_to_2d(self, Xc, Yx, Zc):
         x = numpy.round((Xc * self.fx / Zc) + self.cx)
         y = numpy.round((Yx * self.fy / Zc) + self.cy)
         return x, y
-
-    def on_transform_2_modified(self, observer, eventid):
-        """This method is the observer method for changes in the second transform position"""
-
-        # matrix_2, transform_2 = self.create_4x4_vtk_mat_from_node(self.realWorldTransformNode2)
-        #
-        # x, y, z = get_3d_coordinates(transform_2)
-        #
-        # x_camera, y_camera = self.transform_3d_to_2d(x, y, z)
-        #
-        # # Save to collection
-        # self._marker_2_collection['time'].append(time.time())
-        # self._marker_2_collection['3D pos'].append([x, y, z])
-        # self._marker_2_collection['2D pos'].append([x_camera, y_camera])
-        #
-        # # Setup Marker 2 Matrix
-        # vtk_marker_matrix_2 = self.create_4x4_vtk_mat(x_camera, y_camera)
-
-        #self.marker2Sphere.SetMatrixTransformToParent(vtk_marker_matrix_2)
 
     @staticmethod
     def create_4x4_vtk_mat_from_node(node):
@@ -368,8 +394,7 @@ class displayerLogic(ScriptedLoadableModuleLogic):
         vtk_sp_matrix.DeepCopy(sp_matrix)
         return vtk_sp_matrix
 
-    def run(self, transformOfInterest, fiducialOfInterest, realWorldTransformNode, realWorldTransferNode2,
-            directory_path, cylinder_nodes):
+    def run(self, transformOfInterest, fiducialOfInterest, directory_path, cylinder_nodes):
         # Get Spheres for display
         self.displayMarkerSphere = slicer.mrmlScene.GetFirstNodeByName('Marker_Sphere')
         self.startPointSphere = slicer.mrmlScene.GetFirstNodeByName('Sphere_Transform')
@@ -377,10 +402,6 @@ class displayerLogic(ScriptedLoadableModuleLogic):
 
         self.transformNodeObserverTags = []
         self.transformOfInterestNode = transformOfInterest
-
-        # Make the transform from camera origin to marker origin in the real world available for use in this class
-        self.realWorldTransformNode = realWorldTransformNode
-        self.realWorldTransformNode2 = realWorldTransferNode2
 
         self.display_marker_cylinders = cylinder_nodes
 
@@ -404,22 +425,14 @@ class displayerLogic(ScriptedLoadableModuleLogic):
         #print('coord', coord)
         # Multiply to put start point relative to marker model origin
         self.spInMarker = matrix.MultiplyPoint(coord)
-        #print('hello', self.spInMarker.__str__())
-
-        # Rotate the start point in CT space to match the real world space
-        fix_rotation_matrix = [[-1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]]
-        self.spInMarker = numpy.matmul(fix_rotation_matrix, self.spInMarker)
-        #print('hello again', self.spInMarker.__str__())
-        # Add the offset to cube face
-        # self.spInMarker[1] = self.spInMarker[1] + (18 / 2)
-
-        #print('spInMarker', self.spInMarker)
         self.ctTransform = [[1, 0, 0, self.spInMarker[0]], [0, 1, 0, self.spInMarker[1]], [0, 0, 1, self.spInMarker[2]],
                             [0, 0, 0, 1]]
 
 
         #self.on_transform_2_modified(0, 0)
         # start the updates
+        self.addTransforms()
+        self.updateOffsets()
         self.addObservers()
         self.onTransformOfInterestNodeModified(0, 0)
         # For saving data:
